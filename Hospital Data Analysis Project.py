9d8779fe0024dc3e5e7a5cb90e45883229df8282
vemireddysivaprasad567-source <vemireddysivaprasad567@gmail.com>
2026-03-28 15:27:17 +0530

    Update Hospital Data Analysis Project.py

--- Hospital Data Analysis Project.py
+++ Hospital Data Analysis Project.py
@@ -12,33 +12,22 @@ data = {
     "Doctor":["Dr.A","Dr.B","Dr.C","Dr.A","Dr.D","Dr.A","Dr.B","Dr.C"],
     "Bill_Amount":[2000,5000,12000,2500,20000,3000,6000,11000],
     "Stay_Days":[2,5,7,3,10,4,6,5]
-}
-
-# Create DataFrame
+}
 df = pd.DataFrame(data)
 
 print("----- Hospital Dataset -----")
-print(df)
-
-# Basic Analysis
+print(df)
 print("\nTotal Patients:", df.shape[0])
 print("Average Age:", df["Age"].mean())
 print("Total Hospital Revenue:", df["Bill_Amount"].sum())
 print("Average Stay Days:", df["Stay_Days"].mean())
-
-# Disease Distribution
 print("\nDisease Count")
 print(df["Disease"].value_counts())
-
-# Doctor Performance
 print("\nDoctor Patient Count")
-print(df["Doctor"].value_counts())
-
-# Highest Bill
+print(df["Doctor"].value_counts())
 print("\nHighest Bill Patient")
 print(df.loc[df["Bill_Amount"].idxmax()])
-
-# Visualization
+
 df["Disease"].value_counts().plot(kind="bar")
 
 plt.title("Hospital Disease Distribution")
